0f92d02ee9822963ded324a2ffd75a497a467bb9
ayagaidi <ayagaidi1995@gmail.com>
2022-12-04 12:20:00 +0200

    first

---
+++ CitySeeder.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class CitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('cities')->insert([
+            'name' => 'طرابلس',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'بنغازي',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'مصراته',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'الزاوية',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'زليتن',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'البيضا',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'اجدابيا',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'غريان',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'طبرق',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'صبراته',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'سبها',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'الخمس',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'درنه',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'سرت',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'الجميل',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'الكفره',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'المرج',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'يفرن',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'ترهونة',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'مسلاته',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'بني وليد',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'صرمان',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'رقدالين',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'الزنتان',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'زواره',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'شحات',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'اوباري',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'الابيار',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'زلطن',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'القبه',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'تاورغا',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'المايه',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'مرزق',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'البريقه',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'هون',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'جالو',
+        ]);
+
+        DB::table('cities')->insert([
+            'name' => 'نالوت',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'سلوق',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'مزده',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'راس لانوف',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'العربان',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'ودان',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'العجيلات',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'توكره',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'براك',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'غدامس',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'غات',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'اوجله',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'سوسه',
+        ]);
+        DB::table('cities')->insert([
+            'name' => 'ربيانه',
+        ]);
+    }
+}
